af92d6b43d0d63b06b44e472f0c1890ac54f96fb
Alexander <al791ex08@gmail.com>
2024-04-10 13:35:13 -0400

    borrando astro viejo

--- src/api/lineaPedidos.api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-const LineaPedidosApi = axios.create({
-    baseURL: "http://localhost:8000/pedidos/api/v1/lineapedidos/"
-});
-
-export const AgregarProductoPedido = (productopedido) => LineaPedidosApi.post("/", productopedido);
--- src/api/pedido.api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const PedidosApi = axios.create({
-    baseURL: "http://localhost:8000/pedidos/api/v1/pedidos/"
-});
-
-export const CrearPedido = (pedido) => {
-    return pedidosApi.post("/", pedido);
-};
