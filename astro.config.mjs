f3693311c96863683eb27739b68148f6b6e766e3
Jose2122 <124647625+Jose2122@users.noreply.github.com>
2024-04-11 14:48:15 -0400

    configuración url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
 import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react({
-    esbuildOptions: {
-      // Explicitly set the loader for .js files to interpret them as JSX
-      loader: { '.js': 'jsx' }
-    }
-  })],
-  site: 'https://creativetimofficial.github.io',
-  base: '/delipostres'
   integrations: [react()],
   site: 'https://Al791ex.github.io',
-  base: '/Delipostres-Astro',
+  base: '/delipostres',
 });
